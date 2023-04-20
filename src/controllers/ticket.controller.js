96f39a37292f0c2881e4cfd51bab74fdf6697fc8
sanjayvh <hawaldar.sanjayr.p@gmail.com>
2023-04-20 14:55:59 +0530

    Added restrictions on update and get ticket

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -1,6 +1,6 @@
 const User = require("./../models/user.model");
 const Ticket = require("./../models/ticket.model");
-const constants = require("./../utils/constants");
+const { userTypes, userStatus } = require("./../utils/constants");
 const objectConverter = require("./../utils/objectConverter");
 
 /**
@@ -15,38 +15,40 @@ exports.createTicket = async (req, res) => {
         status: req.body.status,
         // req.userId is not passed in payload but is added by
         // decoding token of user requesting to create ticket
-        reporter: req.userId, 
+        reporter: req.userId,
     };
 
     const engineer = await User.findOne({
-        userType: constants.userTypes.engineer,
-        userStatus: constants.userStatus.approved,
+        userType: userTypes.engineer,
+        userStatus: userStatus.approved,
     });
-    
+
     ticketObject.assignee = engineer.userId;
-    
+
     try {
         const ticket = await Ticket.create(ticketObject);
-        
+
         if (ticket) {
             const user = await User.findOne({
                 userId: req.userId,
             });
-            
+
             user.ticketsCreated.push(ticket._id);
             engineer.ticketsAssigned.push(ticket._id);
-            
+
             await user.save();
             await engineer.save();
 
             res.status(200).send({
-                details: objectConverter.ticketResponse([ticket])
+                details: objectConverter.ticketResponse([ticket]),
             });
         }
     } catch (err) {
         console.log("Error while creating ticket");
         return res.status(500).send({
-            message: "Some internal error seen while creating ticket: " + err.message,
+            message:
+                "Some internal error seen while creating ticket: " +
+                err.message,
         });
     }
 };
@@ -55,37 +57,93 @@ exports.createTicket = async (req, res) => {
  * Only the user who created the ticket is allowed to update the ticket
  */
 exports.updateTicket = async (req, res) => {
-    const ticket = await Ticket.findOne({
-        _id: req.params.id
+    let ticket;
+
+    try {
+        ticket = await Ticket.findOne({
+            _id: req.params.id,
+        });
+
+        if (!ticket) {
+            return res.status(404).send({
+                message: "Ticket not found",
+            });
+        }
+    } catch (err) {
+        console.log("Error while fetching ticket: " + err.message);
+        return res.status(500).send({
+            message: "Internal error while fetching ticket",
+        });
+    }
+        
+    const user = await User.findOne({
+        userId: req.userId,
     });
-    
-    if (ticket.reporter == req.userId) {
+
+    if (
+        ticket.reporter == req.userId ||
+        ticket.assignee == req.userId ||
+        userTypes.admin == user.userType
+    ) {
+        // Allowed to update the ticket
         ticket.title = req.body.title ? req.body.title : ticket.title;
-        ticket.description = req.body.description ? req.body.description : ticket.description;
-        ticket.ticketPriority = req.body.ticketPriority ? req.body.ticketPriority : ticket.ticketPriority;
+        ticket.description = req.body.description
+            ? req.body.description
+            : ticket.description;
+        ticket.ticketPriority = req.body.ticketPriority
+            ? req.body.ticketPriority
+            : ticket.ticketPriority;
         ticket.status = req.body.status ? req.body.status : ticket.status;
-        
+
+        // Only engineer or admin can reassign the ticket to another engineer
+        if (req.body.assignee && (userTypes.engineer == user.userType || userTypes.admin == user.userType))
+            ticket.assignee = req.body.assignee ? req.body.assignee : ticket.assignee;
+
         var updatedTicket = await ticket.save();
-        
+
         res.status(200).send(objectConverter.ticketResponse([updatedTicket]));
     } else {
         console.log("Unauthorized user tried to update the ticket");
         res.status(400).send({
-            message: "Ticket can be updated only by the customer who created it",
+            message:
+                "Ticket can be updated only by the customer who created it or engineer assigned to it",
         });
     }
 };
 
 exports.getAllTickets = async (req, res) => {
-    const queryObj = {
-        reporter: req.userId,
-    }
+    /**
+     * First find the type of user
+     * 1. ADMIN should get the list of all the tickets in the decreasing order of creation time
+     * 2. Customer should only get the tickets created by the him/her.
+     * 3. Engineer should be able to get the tickets assigned to him/her or created by him/her.
+     */
+
+    const queryObj = {};
 
     if (req.query.status) {
         queryObj.status = req.query.status;
     }
-    
+
+    const savedUser = await User.findOne({
+        userId: req.userId,
+    });
+
+    if (savedUser.userType == userTypes.engineer) {
+        queryObj.assignee = req.userId;
+    } else if (savedUser.userType == userTypes.customer) {
+        queryObj.reporter = req.userId;
+    }
+
     const tickets = await Ticket.find(queryObj);
-    
+
     res.status(200).send(objectConverter.ticketResponse(tickets));
+};
+
+exports.getATicket = async (req, res) => {
+    const ticket = await Ticket.findOne({
+        _id: req.params.id,
+    });
+
+    res.status(200).send(objectConverter.ticketResponse([ticket]));
 };
--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -5,4 +5,5 @@ module.exports = function(app) {
     app.post('/crm/api/v1/tickets/create', [authJwt.verifyToken, verifyTicketReqBody.validateTicketRequestBody], ticketController.createTicket);
     app.put('/crm/api/v1/tickets/:id', [authJwt.verifyToken, verifyTicketReqBody.validateTicketStatus], ticketController.updateTicket);
     app.get('/crm/api/v1/tickets', [authJwt.verifyToken], ticketController.getAllTickets);
+    app.get('/crm/api/v1/tickets/:id', [authJwt.verifyToken], ticketController.getATicket);
 }
