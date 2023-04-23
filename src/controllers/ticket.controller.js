d6610f3bead06cf00c98b8078463415b0e5058c3
sanjayvh <hawaldar.sanjayr.p@gmail.com>
2023-04-23 16:35:22 +0530

    Integration changes for crm and notification service

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -2,6 +2,7 @@ const User = require("./../models/user.model");
 const Ticket = require("./../models/ticket.model");
 const { userTypes, userStatus } = require("./../utils/constants");
 const objectConverter = require("./../utils/objectConverter");
+const sendEmail = require("../utils/notificationClient");
 
 /**
  * Create a ticket
@@ -39,6 +40,11 @@ exports.createTicket = async (req, res) => {
             await user.save();
             await engineer.save();
 
+            /**
+             *  Sending the notification to the assigned engineer in async manner.
+             */
+            sendEmail(ticket._id, "Ticket with id: " + ticket._id + " created", ticket.description, user.email + "," + engineer.email, user.email);
+            
             res.status(200).send({
                 details: objectConverter.ticketResponse([ticket]),
             });
@@ -47,7 +53,7 @@ exports.createTicket = async (req, res) => {
         console.log("Error while creating ticket");
         return res.status(500).send({
             message:
-                "Some internal error seen while creating ticket: " +
+                "Some internal error seen while creating ticket: " + 
                 err.message,
         });
     }
@@ -55,15 +61,15 @@ exports.createTicket = async (req, res) => {
 
 /**
  * Only the user who created the ticket is allowed to update the ticket
- */
+*/
 exports.updateTicket = async (req, res) => {
     let ticket;
-
+    
     try {
         ticket = await Ticket.findOne({
             _id: req.params.id,
         });
-
+        
         if (!ticket) {
             return res.status(404).send({
                 message: "Ticket not found",
@@ -75,33 +81,43 @@ exports.updateTicket = async (req, res) => {
             message: "Internal error while fetching ticket",
         });
     }
-        
+    
     const user = await User.findOne({
         userId: req.userId,
     });
-
+    
     if (
         ticket.reporter == req.userId ||
         ticket.assignee == req.userId ||
         userTypes.admin == user.userType
-    ) {
-        // Allowed to update the ticket
-        ticket.title = req.body.title ? req.body.title : ticket.title;
-        ticket.description = req.body.description
+        ) {
+            // Allowed to update the ticket
+            ticket.title = req.body.title ? req.body.title : ticket.title;
+            ticket.description = req.body.description
             ? req.body.description
             : ticket.description;
-        ticket.ticketPriority = req.body.ticketPriority
+            ticket.ticketPriority = req.body.ticketPriority
             ? req.body.ticketPriority
             : ticket.ticketPriority;
-        ticket.status = req.body.status ? req.body.status : ticket.status;
-
-        // Only engineer or admin can reassign the ticket to another engineer
-        if (req.body.assignee && (userTypes.engineer == user.userType || userTypes.admin == user.userType))
+            ticket.status = req.body.status ? req.body.status : ticket.status;
+            
+            // Only engineer or admin can reassign the ticket to another engineer
+            if (req.body.assignee && (userTypes.engineer == user.userType || userTypes.admin == user.userType))
             ticket.assignee = req.body.assignee ? req.body.assignee : ticket.assignee;
-
-        var updatedTicket = await ticket.save();
-
-        res.status(200).send(objectConverter.ticketResponse([updatedTicket]));
+            
+            var updatedTicket = await ticket.save();
+            
+            const engineer = await User.findOne({
+                userId: ticket.assignee
+            });
+            
+            const reporter = await User.findOne({
+                userId: ticket.reporter
+            });
+            
+            sendEmail(ticket._id, "Ticket with id: " + ticket._id + " updated", ticket.description, user.email + "," + engineer.email + "," + reporter.email, user.email);
+            
+            res.status(200).send(objectConverter.ticketResponse([updatedTicket]));
     } else {
         console.log("Unauthorized user tried to update the ticket");
         res.status(400).send({
---
+++ src/utils/notificationClient.js
@@ -0,0 +1,26 @@
+var Client = require("node-rest-client").Client;
+var client = new Client();
+
+module.exports = (ticketId, subject, content, emailIds, requestor) => {
+    var reqBody = {
+        subject: subject,
+        content: content,
+        recipientEmails: emailIds,
+        requestor: requestor,
+        ticketId: ticketId
+    };
+
+    var args = {
+        data: reqBody,
+        headers: {"Content-Type": "application/json"}
+    };
+
+    /**
+     * Now we will send post request to the notification service
+     */
+
+    client.post(process.env.NOTIFICATION_URL, args, function (data, response) {
+        console.log("Post Request Sent to the Notification Service");
+        console.log(data);
+    });
+};
